36c9a5362bae67fea129ede56d360d317e73aba2
becoze <liyuanliang20@outlook.com>
2023-12-13 21:43:30 +1100

    Initial commit

---
+++ src/pages/User/Login/index.tsx
@@ -0,0 +1,165 @@
+import { Footer } from '@/components';
+import {
+  LockOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import {
+  LoginForm,
+  ProFormText,
+} from '@ant-design/pro-components';
+import { useEmotionCss } from '@ant-design/use-emotion-css';
+import { Helmet, history, useModel } from '@umijs/max';
+import { Tabs, message } from 'antd';
+import React, {useEffect, useState} from 'react';
+import { flushSync } from 'react-dom';
+import Settings from '../../../../config/defaultSettings';
+import {listChartByPageUsingPOST} from "@/services/bi_front/chartController";
+import {Link} from "umi";
+import {getLoginUserUsingGET, userLoginUsingPOST} from "@/services/bi_front/userController";
+
+const Login: React.FC = () => {
+  const [type, setType] = useState<string>('account');
+  const { initialState0, setInitialState } = useModel('@@initialState');
+  const containerClassName = useEmotionCss(() => {
+    return {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      overflow: 'auto',
+      backgroundImage:
+        "url('https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr')",
+      backgroundSize: '100% 100%',
+    };
+  });
+
+  useEffect(()=>{
+    listChartByPageUsingPOST({}).then(res => {
+      console.error('res', res)
+    })
+  })
+
+
+  const fetchUserInfo = async () => {
+    const userInfo = await getLoginUserUsingGET();
+    if (userInfo) {
+      flushSync(() => {
+        setInitialState((s) => ({
+          ...s,
+          currentUser: userInfo,
+        }));
+      });
+    }
+  };
+  const handleSubmit = async (values: API.UserLoginRequest) => {
+    try {
+      // 登录
+      const res = await userLoginUsingPOST(values);
+      if (res.code === 0) {
+        const defaultLoginSuccessMessage = 'Login successful!';
+        message.success(defaultLoginSuccessMessage);
+        await fetchUserInfo();
+        const urlParams = new URL(window.location.href).searchParams;
+        history.push(urlParams.get('redirect') || '/');
+        return;
+      } else {
+        message.error(res.message);
+      }
+    } catch (error) {
+      const defaultLoginFailureMessage = 'Login failed, please try again!';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+
+  return (
+    <div className={containerClassName}>
+      <Helmet>
+        <title>
+          {'Login'}- {Settings.title}
+        </title>
+      </Helmet>
+      <div
+        style={{
+          flex: '1',
+          padding: '32px 0',
+        }}
+      >
+        <LoginForm
+          contentStyle={{
+            minWidth: 280,
+            maxWidth: '75vw',
+          }}
+          logo={<img alt="logo" src="/logo.svg" />}
+          title="Beco_Intelligent BI"
+          subTitle={
+            <Link key="subTitle" to="https://www.linkedin.com/in/liyuan-liang/" target="_blank">
+              <span>Beco_Intelligent BI Liyuan liang Becoze</span>
+            </Link>
+            // <a href="https://www.linkedin.com/in/liyuan-liang/" target="_blank"> Beco_Intelligent BI Liyuan liang Becoze </a>
+          }
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserLoginRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: 'Account Login',
+              },
+            ]}
+          />
+
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined />,
+                }}
+                placeholder={'User name'}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined />,
+                }}
+                placeholder={'User password'}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+              />
+            </>
+          )}
+
+          <div
+            style={{
+              marginBottom: 24,
+            }}
+          >
+            <Link to="/user/register">
+              Register
+            </Link>
+
+          </div>
+        </LoginForm>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default Login;
